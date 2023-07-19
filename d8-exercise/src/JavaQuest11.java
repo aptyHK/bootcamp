85e126004d4cb4b73302079b5966db72d75bbffb
andy.yip <apty0911@gmail.com>
2023-07-19 21:01:16 +0800

    update day 8 exercise

---
+++ d8-exercise/src/JavaQuest11.java
@@ -0,0 +1,31 @@
+/**
+ * Input: arrivalTime = 15, delayedTime = 5
+ * Output: 20 Explanation: Reach at 15+5 = 20 (20:00).
+ *
+ */
+public class JavaQuest11 {
+  // You are given a positive integer arrivalTime denoting the arrival time of a
+  // train in hours,
+  // and another positive integer delayedTime denoting the amount of delay in
+  // hours.
+  // Calculate Delayed Arrival Time
+  public static int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+    // code here ...
+    int time = arrivalTime + delayedTime;
+    if ((arrivalTime + delayedTime) >= 24) {
+      time = time % 24;
+    }
+    return time;
+  }
+
+  public static void main(String[] args) {
+    int delayedArrivalTime1 = findDelayedArrivalTime(15, 5); // 20
+    int delayedArrivalTime2 = findDelayedArrivalTime(23, 5); // 4
+    int delayedArrivalTime3 = findDelayedArrivalTime(18, 6); // 0
+
+    System.out.println(delayedArrivalTime1);
+    System.out.println(delayedArrivalTime2);
+    System.out.println(delayedArrivalTime3);
+
+  }
+}
